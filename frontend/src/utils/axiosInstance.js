1aefa161947cf0d7374ccbe5981f41cebbde1177
Jared Busano <jaredbusano2@gmail.com>
2026-07-13 20:56:07 +0800

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "https://restaurantms-api-production.up.railway.app/",
 });
 
 axiosInstance.interceptors.request.use(
